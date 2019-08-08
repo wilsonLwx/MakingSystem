9773137d4fa9361500ee1da51791337b2dc1c378
haowx <hw121298@163.com>
2019-08-09 00:20:11 +0800

    [User] user login init

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,6 +5,16 @@ from django.db import models
 # Create your models here.
 
 class Users(AbstractUser):
+    real_name = models.CharField(max_length=10)
+    moblie = models.IntegerField(max_length=11)
+    gender = models.BooleanField(default=0)
+    email = models.EmailField(max_length=20, blank=True, null=True)
+    school = models.CharField(max_length=20)
     name = models.CharField(max_length=10)
-    email = models.EmailField(max_length=20)
     token = models.CharField(max_length=30)
+    profession = models.CharField(max_length=20)
+    number = models.IntegerField(max_length=20)
+    date_birth = models.DateField()
+    grade = models.CharField(max_length=10)
+    education = models.CharField(max_length=10)
+    vocation = models.CharField(max_length=20)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django import db
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.generic import View
+
+
+class RegisterView(View):
+    """
+    执行注册逻辑
+    """
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ActiveView(View):
+    """
+    激活用户
+    """
+    def get(self, request, token):
+        pass
+
+
+class LoginView(View):
+    """
+    返回登录界面
+    """
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+class LogoutView(View):
+    """
+    登出功能
+    """
+
+    def get(self, request):
+        pass
+
