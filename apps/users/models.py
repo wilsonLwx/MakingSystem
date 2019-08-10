f615cdbb73e2e3793395a38a99dbacd126e74d2e
wilsonLWX <15000108650@163.com>
2019-08-10 14:39:52 +0800

    change the pwd

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,17 +4,24 @@ from django.db import models
 
 # Create your models here.
 
+
 class Users(AbstractUser):
     real_name = models.CharField(max_length=10)
-    moblie = models.IntegerField(max_length=11)
-    gender = models.BooleanField(default=0)
+    mobile = models.CharField(max_length=11)
+    gender = models.BooleanField(default=False)
     email = models.EmailField(max_length=20, blank=True, null=True)
     school = models.CharField(max_length=20)
     name = models.CharField(max_length=10)
     token = models.CharField(max_length=30)
     profession = models.CharField(max_length=20)
-    number = models.IntegerField(max_length=20)
-    date_birth = models.DateField()
+    number = models.IntegerField()
+    date_birth = models.DateField(auto_now_add=True)
     grade = models.CharField(max_length=10)
     education = models.CharField(max_length=10)
     vocation = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'Users'
+
+    def __str__(self):
+        return Users.real_name
