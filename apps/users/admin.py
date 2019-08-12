d164b359d31554be35c24e3302262c06cb85bbde
李阳 <liyang@liyangdeMBP.lan>
2019-08-12 22:02:58 +0800

    add admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Users, ClassBanner, Banner, LeaderBanner
+
 
 # Register your models here.
+
+class ClassBannerAdmin(admin.ModelAdmin):
+    """课程分类admin后台管理"""
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+
+
+class BannerAdmin(admin.ModelAdmin):
+    """首页轮播图admin后台管理"""
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+
+
+class LeaderBannerAdmin(admin.ModelAdmin):
+    """领导分类图admin后台管理"""
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+
+
+admin.site.register(ClassBanner, ClassBannerAdmin)
+admin.site.register(Banner, BannerAdmin)
+admin.site.register(LeaderBanner, LeaderBannerAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -24,3 +26,39 @@ class Users(AbstractUser):
 
     def __str__(self):
         return Users.name
+
+
+class Banner(models.Model):
+    title = models.CharField('标题', max_length=100)
+    image = models.ImageField('轮播图', upload_to='banner/%Y%m', max_length=100)
+    url = models.URLField('访问地址', max_length=200)
+    index = models.IntegerField('顺序', default=100)
+    add_time = models.DateTimeField('添加时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
+
+
+class ClassBanner(models.Model):
+    title = models.CharField('标题', max_length=100)
+    image = models.ImageField('图片', upload_to='banner/%Y%m', max_length=100)
+    url = models.URLField('访问地址', max_length=200)
+    index = models.IntegerField('顺序', default=100)
+    add_time = models.DateTimeField('添加时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '课程分类图'
+        verbose_name_plural = verbose_name
+
+
+class LeaderBanner(models.Model):
+    title = models.CharField('标题', max_length=100)
+    image = models.ImageField('图片', upload_to='banner/%Y%m', max_length=100)
+    url = models.URLField('访问地址', max_length=200)
+    index = models.IntegerField('顺序', default=100)
+    add_time = models.DateTimeField('添加时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '领导分类图'
+        verbose_name_plural = verbose_name
