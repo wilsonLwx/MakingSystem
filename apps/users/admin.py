2df12a2a55cf4d978941bc205296a023a479bb65
ly18551772479 <1136862540@qq.com>
2019-08-14 14:19:20 +0800

    add admin to dev

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Banner, Users, ClassBanner
+
+
+class BannerAdmin(admin.ModelAdmin):
+    """评论轮播图"""
+    list_display = ['id', 'title', 'url', 'index', 'add_time']
+
+
+class UsersAdmin(admin.ModelAdmin):
+    """用户"""
+    list_display = ['id', 'username', 'real_name', 'email', 'mobile',
+                    'gender', 'school', 'profession', 'education', 'vocation']
+
+
+class ClassBannerAdmin(admin.ModelAdmin):
+    """课程分类轮播图"""
+    list_display = ['id', 'title', 'url', 'index', 'add_time']
+
+
+admin.site.register(Banner, BannerAdmin)
+admin.site.register(ClassBanner, ClassBannerAdmin)
+admin.site.register(Users, UsersAdmin)
+
+admin.site.site_header = '管理后台'
+admin.site.site_title = '大学生领导力潜质研究院'
+
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -6,7 +8,6 @@ from django.db import models
 
 
 class Users(AbstractUser):
-
     gender_choices = (
         ('male', '男'),
         ('female', '女')
@@ -14,7 +15,7 @@ class Users(AbstractUser):
 
     real_name = models.CharField("真实姓名", max_length=10, null=True)
     mobile = models.CharField("手机号", max_length=11)
-    gender = models.BooleanField("性别",choices=gender_choices, default="male")
+    gender = models.BooleanField("性别", choices=gender_choices, default="male")
     email = models.EmailField("邮箱", max_length=20, blank=True, null=True)
     school = models.CharField("学校", max_length=20, null=True)
     name = models.CharField("昵称", max_length=10)
@@ -30,3 +31,28 @@ class Users(AbstractUser):
 
     def __str__(self):
         return Users.name
+
+
+class Banner(models.Model):
+    title = models.CharField('标题', max_length=100)
+    image = models.ImageField('轮播图', upload_to='banner/%Y%m', max_length=100)
+    url = models.URLField('访问地址', max_length=200)
+    index = models.IntegerField('顺序', default=100)
+    add_time = models.DateTimeField('添加时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = '评估轮播图'
+
+
+class ClassBanner(models.Model):
+    title = models.CharField('标题', max_length=100)
+    image = models.ImageField('轮播图', upload_to='classbanner/%Y%m', max_length=100)
+    # url = models.URLField('访问地址', max_length=200, default='https://www.baidu.com/')
+    url = models.URLField('访问地址', max_length=200)
+    index = models.IntegerField('顺序', default=100)
+    add_time = models.DateTimeField('添加时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = '课程分类轮播图'
