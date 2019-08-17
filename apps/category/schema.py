3862461547bb7ad24f97a3196f1d9e050438fc6d
wilsonLWX <15000108650@163.com>
2019-08-17 13:29:31 +0800

    ss

--- apps/category/schema.py
+++ apps/category/schema.py
@@ -4,21 +4,25 @@
 import graphene
 from graphene_django import DjangoObjectType
 
+from makingsystem.settings import MEDIA_ROOT
 from .models import TestDetails as TestDetailsModel
 from .models import TestName as TestNameModel
 
 
-class TestNameType(graphene.ObjectType):
-    names = graphene.List(graphene.String)
-    urls = graphene.List(graphene.String)
+class LeaderTestInfo(graphene.ObjectType):
+    name = graphene.String()
+    url = graphene.String()
+    image = graphene.String()
+
+
+class LeaderTestType(graphene.ObjectType):
+    group = graphene.List(LeaderTestInfo)
 
 
 class Query:
+    leader_test = graphene.Field(LeaderTestType)
 
-    banners = graphene.Field(TestNameType)
-
-    def resolve_banners(self, info):
+    def resolve_leader_test(self, info):
         banners_obj = TestDetailsModel.objects.filter(is_index_show=True)
-        banner_names = banners_obj.values_list('parent_test_name__name', flat=True)
-        banner_urls = banners_obj.values_list('url', flat=True)
-        return TestNameType(names=banner_names, urls=banner_urls)
+        banner_info = banners_obj.values_list('parent_test_name__name', 'url', 'image')
+        return LeaderTestType(group=[LeaderTestInfo(name=i[0], url=i[1], image=i[2]) for i in banner_info])
