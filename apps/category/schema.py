9fd36ca8cdd1f2e82f1767ce400a9d0032638ff5
wilsonLWX <15000108650@163.com>
2019-08-17 13:40:54 +0800

    ss

--- apps/category/schema.py
+++ apps/category/schema.py
@@ -19,19 +19,10 @@ class LeaderTestType(graphene.ObjectType):
     group = graphene.List(LeaderTestInfo)
 
 
-class BannerType(DjangoObjectType):
-    class Meta:
-        model = BannerModel
-
-
 class Query:
     leader_test = graphene.Field(LeaderTestType)
-    banner = graphene.Field(BannerType)
 
     def resolve_leader_test(self, info):
         banners_obj = TestDetailsModel.objects.filter(is_index_show=True, push_time__lt=datetime.now())
         banner_info = banners_obj.values_list('parent_test_name__name', 'url', 'image')
         return LeaderTestType(group=[LeaderTestInfo(name=i[0], url=i[1], image=i[2]) for i in banner_info])
-
-    def resolve_banner(self, info):
-        return BannerModel.objects.filter(is_show=True, push_time__lt=datetime.now())
