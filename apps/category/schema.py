de37fb77414bed527ddf880b56703d5a7c488ee7
wilsonLWX <15000108650@163.com>
2019-08-22 20:01:12 +0800

    sss

--- apps/category/schema.py
+++ apps/category/schema.py
@@ -1,22 +1,11 @@
 # __author__ = 'ly'
 # __date__ = '2019/08/15'
-from datetime import datetime
-import graphene
 
-from .models import Banner as BannerModel
+import graphene
 from .models import TestDetails as TestDetailsModel
 from .models import TestName as TestNameModel
 from .models import Banner as BannerModel
 from datetime import datetime
-from makingsystem.settings.base import MEDIA_ROOT
-
-
-# class LeaderTestInfo(graphene.ObjectType):
-#     name = graphene.String()
-#     url = graphene.String()
-#     title = graphene.String()
-#     image = graphene.String()
-#     des = graphene.String()
 
 
 class LeaderTestInfo(graphene.ObjectType):
@@ -29,25 +18,10 @@ class LeaderTestType(graphene.ObjectType):
     group = graphene.List(LeaderTestInfo)
 
 
-<<<<<<< HEAD
-class Query:
+class Query(graphene.ObjectType):
     leader_test = graphene.Field(LeaderTestType)
     courses = graphene.Field(LeaderTestType)
     indexbanners = graphene.Field(LeaderTestType)
-=======
-class BannerType(LeaderTestType):
-    pass
-
-
-class IndexBannerType(LeaderTestType):
-    pass
-
-
-class Query(graphene.ObjectType):
-    leader_test = graphene.Field(LeaderTestType)
-    courses = graphene.Field(BannerType)
-    indexbanners = graphene.Field(IndexBannerType)
->>>>>>> 215ccbc5a3588fb4fa8f3a0aa6018a9ca1659028
 
     def resolve_leader_test(self, info):
         leader_test_obj = TestDetailsModel.objects.filter(is_index_show=True, push_time__lt=datetime.now())
@@ -58,7 +32,7 @@ class Query(graphene.ObjectType):
         # 首页---课程分类
         banner_obj = BannerModel.objects.filter(is_show=True, push_time__lt=datetime.now())
         banner_info = banner_obj.values_list('title', 'url', 'image')
-        return BannerType(group=[LeaderTestInfo(title=i[0], url=i[1], image=i[2]) for i in banner_info])
+        return LeaderTestType(group=[LeaderTestInfo(title=i[0], url=i[1], image=i[2]) for i in banner_info])
 
     def resolve_indexbanners(self, info):
         # 首页 第一部分 轮播图
